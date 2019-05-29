c2b73a36647c53904bac8fff39f3d92cfcb2ca73
Michael Starke <michael.starke@hicknhack-software.com>
2019-05-29 08:53:23 +0200

    Removed build folder

--- MacPassRevealer/build/MacPassRevealer.build/Release/MacPassRevealer.build/Objects-normal/x86_64/MacPassRevealer.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/georgesnow/Library/Mobile\ Documents/com~apple~CloudDocs/programming/xcode/devMacPass/MacPassRevealer/MacPassRevealer/MacPassRevealer.m \
-  /Users/georgesnow/Library/Mobile\ Documents/com~apple~CloudDocs/programming/xcode/devMacPass/MacPassRevealer/MacPassRevealer/MacPassRevealer.h \
-  ../MacPass/MacPass/MPPlugin.h
