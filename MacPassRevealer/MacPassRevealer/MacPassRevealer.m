a8bd523185a5aa482a921b0ef4da063c9e69d9dd
georgesnow <gsnowiii@gmail.com>
2019-05-31 08:55:58 -0400

    more reliable toggle - menu not displaying
    fixed the reliableness of toggling MacPass window. and it now properly detects an option click and gets to createstatusmenubar. however, menu doesn't seem to display or build the menu

--- MacPassRevealer/MacPassRevealer/MacPassRevealer.m
+++ MacPassRevealer/MacPassRevealer/MacPassRevealer.m
@@ -20,6 +20,7 @@ static int const MPRHotKeyId2 = 2;
 
 @end
 
+
 @implementation MPRMacPassRevealer
 
 //hotkey
@@ -57,13 +58,8 @@ OSStatus OnHotKeyEvent(EventHandlerCallRef nextHandler,EventRef theEvent,void *u
   if(self) {
     [self registerHotKeys];
     [self createStatusBarItem];
-    //self.statusItem = [NSStatusBar.systemStatusBar statusItemWithLength:NSVariableStatusItemLength];
-    //NSBundle *bundle = [NSBundle bundleForClass:self.class];
-    // TODO: Use scalable graphic (e.g. PDF)
-    //self.statusItem.button.image = [bundle imageForResource:@"Lock3"];
-    //self.statusItem.button.toolTip = @"Option+Click Quits MacPass";
-    //self.statusItem.button.action = @selector(itemClicked:);
-    //self.statusItem.button.target = self;
+ 
+    _statusItem.button.action = @selector(itemClicked:);
     
     // TODO: Make hide dock icon user pref - needs refinement
     //[NSApp setActivationPolicy:NSApplicationActivationPolicyAccessory];
@@ -72,18 +68,17 @@ OSStatus OnHotKeyEvent(EventHandlerCallRef nextHandler,EventRef theEvent,void *u
 }
 
 
-- (void)createStatusBarItem {
+- (void)createStatusBarItem{
   NSStatusBar *statusBar = [NSStatusBar systemStatusBar];
   NSBundle *bundle = [NSBundle bundleForClass:self.class];
   _statusItem = [statusBar statusItemWithLength:NSSquareStatusItemLength];
   _statusItem.button.image = [bundle imageForResource:@"Lock3"];
-  _statusItem.button.action = @selector(itemClicked:);
   _statusItem.button.target = self;
-  //_statusItem.menu = [self createStatusBarMenu];
   
 }
 
 - (void)activateMacPass:(id)sender {
+  //_statusItem.menu = nil;
   NSRunningApplication *frontMostApplication = NSWorkspace.sharedWorkspace.frontmostApplication;
   NSRunningApplication *macPass = NSRunningApplication.currentApplication;
   if(frontMostApplication.processIdentifier == macPass.processIdentifier) {
@@ -91,19 +86,26 @@ OSStatus OnHotKeyEvent(EventHandlerCallRef nextHandler,EventRef theEvent,void *u
   }
   else {
     [NSApplication.sharedApplication activateIgnoringOtherApps:YES];
+    
   }
 }
 
-- (NSMenu *)createStatusBarMenu {
+- (void)createStatusBarMenu:(NSMenu *)menu {
 
-   NSMenu *menu = [[NSMenu alloc] init];
-  
-  NSMenuItem *quitMacPass = [[NSMenuItem alloc] initWithTitle:@"Quit MacPass" action:@selector(quitMacPass:) keyEquivalent:@""];
+  //NSMenu *menu = [[NSMenu alloc] init];
+  NSLog(@"made it to createStatusBarMenu");
+  NSMenuItem *quitMacPass = [[NSMenuItem alloc] initWithTitle:@"Quit MacPass" action:@selector(quitMacPass:) keyEquivalent:@"quitMacPass"];
   [quitMacPass setTarget:self];
+  [_statusItem.menu addItem:quitMacPass];
   [menu addItem:quitMacPass];
+  _statusItem.menu = menu;
   
-  return menu;
   
+}
+- (NSMenu *)clearStatusMenu {
+  NSMenu *menu = [[NSMenu alloc] init];
+  [menu removeAllItems];
+  return nil;
 }
 
 - (void)quitMacPass:(id)sender {
@@ -113,17 +115,19 @@ OSStatus OnHotKeyEvent(EventHandlerCallRef nextHandler,EventRef theEvent,void *u
 }
 
 - (void)itemClicked:(id)sender {
-  NSLog(@"item clicked triggered");
-  //Look for option click display menu
-  //[[NSStatusBar systemStatusBar] removeStatusItem:self.statusItem];
+  //_statusItem.menu = nil;
   NSEvent *event = [NSApp currentEvent];
-  if([event modifierFlags] & NSEventModifierFlagOption) {
-    NSLog(@"first option+regular click");
-    _statusItem.menu = [self createStatusBarMenu];
+  if (([event modifierFlags] & NSEventModifierFlagOption)) {
+    NSLog(@"option click detected");
+    [self performSelector:@selector(createStatusBarMenu:) withObject:(_statusItem.menu)];
   }
   else {
-    [self performSelector:(@selector(activateMacPass:))];
+    NSLog(@"regularclick detected sending to actiavetMacPass");
+    [self performSelector:@selector(activateMacPass:)];
   }
+  
+
+}
 
 - (void)registerHotKeys {
   EventHotKeyRef gMyHotKeyRef;
