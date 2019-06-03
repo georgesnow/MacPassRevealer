3ad5b1bc8630653d89f98fb9ca01a0e756fa2fe3
georgesnow <gsnowiii@gmail.com>
2019-06-03 08:50:46 -0400

    cleanup
    only incorporates hotkey functionality removed status item menu

--- MacPassRevealer/MacPassRevealer/MacPassRevealer.m
+++ MacPassRevealer/MacPassRevealer/MacPassRevealer.m
@@ -35,32 +35,7 @@
   }
   return self;
   }
-  
-
-
-
-//
-
-- (IBAction)activateMacPass:(id)sender {
-  NSRunningApplication *frontMostApplication = NSWorkspace.sharedWorkspace.frontmostApplication;
-  NSRunningApplication *macPass = NSRunningApplication.currentApplication;
-  if(frontMostApplication.processIdentifier == macPass.processIdentifier) {
-    [NSApplication.sharedApplication hide:nil];
-  }
-  else {
-    [NSApplication.sharedApplication activateIgnoringOtherApps:YES];
-    
-  }
-}
-
-- (IBAction)quitMacPass:(id)sender {
-  [[NSApplication sharedApplication] terminate:self];
-  
-  return;
-}
-
-
-
+ 
 
 @end
 
